5740ad894eddd54724f792f83fc094891c7190ca
SilverFran <Francisco1627@gmail.com>
2020-08-02 18:19:04 +0000

    singup post tested

--- src/main.py
+++ src/main.py
@@ -39,6 +39,37 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    ciphertext = params.get('hash', None)
+    
+    if not ciphertext:
+        return jsonify({"msg": "Missing hash parameter"}), 400
+    
+
+    # email_query = User.query.filter_by(email=email).first()
+    # if email_query is not None:
+    #     return jsonify({"msg": "Email already exist"}), 401
+    
+    # name_query = User.query.filter_by(username=username).first()
+    # if name_query is not None:
+    #     return jsonify({"msg": "User name already exist"}), 401
+
+    # user = User(username=username, email=email, password=password, )
+    # db.session.add(user)
+    # db.session.commit()
+
+    # # Identity can be any data that is json serializable
+    # ret = {'jwt': create_jwt(identity=username), 'lvl': 3}
+    # return jsonify(ret), 200
+
+    response_body = {"msg": "Hello, this is your Hash /singUp response "}
+    return jsonify(response_body), 200
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
